04c49023b30d3bb44be462f108167bab99c68f8b
To Van Lam <tovanlam20132223@gmail.com>
2021-01-31 18:57:26 +0700

    generator_v1

--- tshirt_video_generator.py
+++ tshirt_video_generator.py
@@ -59,8 +59,12 @@ class MyWindow:
         self.end_model=214
         self.start_close=215
         self.end_close=240
-        self.start_text=241
-        self.end_text=473
+        self.start_frame_text=241
+        self.end_frame_text=473
+        self.text_font_size=70
+        self.domain = ""
+
+        self.run_count=0
 
         self.folder_png = cur_dir + "/dataset/folder_png"
 
@@ -84,58 +88,168 @@ class MyWindow:
         self.t1 = Button(text='Chon thu muc chua list design png', bg="gray", fg="blue",
                          command=self.callback_folder_png)
         self.t3 = Button(text='Chon  file video MP4', bg="gray", fg="blue", command=self.callback_file_mp4)
+        self.t3_gen_frame = Button(text='Gen file MP4 to LIST FRAME', bg="gray", fg="blue", command=self.callback_gen_frame_file_mp4)
         self.t4 = Button(text='Chon thu muc output', bg="gray", fg="blue", command=self.callback_folder_output)
 
-        self.lbt1 = Label(win, text='start time image (milisecond): ')
-        self.et1 = Entry(win, bd=5, foreground="green")
-        self.lbt2 = Label(win, text='end time image: (milisecond) ')
-        self.et2 = Entry(win, bd=5, foreground="green")
-        self.lbt3 = Label(win, text='start time link: (milisecond)')
-        self.et3 = Entry(win, bd=5, foreground="green")
-        self.lbt4 = Label(win, text='end time link: (milisecond)')
-        self.et4 = Entry(win, bd=5, foreground="green")
+        self.lbt1open = Label(win, text='start open intro (count): ')
+        self.et1open = Entry(win, bd=5, foreground="green")
+        self.lbt2open = Label(win, text='end open intro: (count) ')
+        self.et2open = Entry(win, bd=5, foreground="green")
 
-        self.et1.insert(END, str(self.start_model))
-        self.et2.insert(END, str(self.end_model))
-        self.et3.insert(END, str(self.start_text))
-        self.et4.insert(END, str(self.end_text))
+        self.lbt1model = Label(win, text='start model (count): ')
+        self.et1model = Entry(win, bd=5, foreground="green")
+        self.lbt2model = Label(win, text='end model: (count) ')
+        self.et2model = Entry(win, bd=5, foreground="green")
+
+        self.lbt1close = Label(win, text='start close intro (count): ')
+        self.et1close = Entry(win, bd=5, foreground="green")
+        self.lbt2close = Label(win, text='end close intro: (count) ')
+        self.et2close = Entry(win, bd=5, foreground="green")
+
+        self.lbt1link = Label(win, text='start time link: (count)')
+        self.et1link = Entry(win, bd=5, foreground="green")
+        self.lbt2link = Label(win, text='end time link: (count)')
+        self.et2link = Entry(win, bd=5, foreground="green")
+
+        self.et1open.insert(END, str(self.start_open))
+        self.et2open.insert(END, str(self.end_open))
+
+        self.et1model.insert(END, str(self.start_model))
+        self.et2model.insert(END, str(self.end_model))
+
+        self.et1close.insert(END, str(self.start_close))
+        self.et2close.insert(END, str(self.end_close))
+
+
+        self.et1link.insert(END, str(self.start_frame_text))
+        self.et2link.insert(END, str(self.end_frame_text))
 
         self.b1 = Button(win, text='RUN', bg="green", fg="white", command=self.start_run)
         self.b2 = Button(win, text='STOP', bg="red", fg="white", command=self.stop_run)
         self.t5 = Label(win, text='KET QUA:')
         self.lbt55 = Label(win, text='number thread')
         self.et55 = Entry(win, bd=2, foreground="green")
+
+        self.lbtdomain = Label(win, text='domain')
+        self.etdomain = Entry(win, bd=2, foreground="red")
+
+        self.lbtfont_size= Label(win, text='text font size')
+        self.etfont_size = Entry(win, bd=2, foreground="red")
+
         self.et55.insert(END, '1')
         self.is_new_background=tk.IntVar(value=1)
         self.c1 = tk.Checkbutton(window, text='is_new_background', variable=self.is_new_background, onvalue=1, offvalue=0)
-        self.lbt1.place(x=130, y=190)
-        self.et1.place(x=305, y=190)
-        self.lbt2.place(x=480, y=190)
-        self.et2.place(x=655, y=190)
-        self.lbt3.place(x=130, y=230)
-        self.et3.place(x=305, y=230)
-        self.lbt4.place(x=480, y=230)
-        self.et4.place(x=655, y=230)
+        self.lbt1open.place(x=130, y=190)
+        self.et1open.place(x=305, y=190)
+        self.lbt2open.place(x=480, y=190)
+        self.et2open.place(x=655, y=190)
+
+        self.lbt1model.place(x=130, y=230)
+        self.et1model.place(x=305, y=230)
+        self.lbt2model.place(x=480, y=230)
+        self.et2model.place(x=655, y=230)
+
+        self.lbt1close.place(x=130, y=270)
+        self.et1close.place(x=305, y=270)
+        self.lbt2close.place(x=480, y=270)
+        self.et2close.place(x=655, y=270)
+
+        self.lbt1link.place(x=130, y=310)
+        self.et1link.place(x=305, y=310)
+        self.lbt2link.place(x=480, y=310)
+        self.et2link.place(x=655, y=310)
+
+
+
         self.lbl0.place(x=0, y=10)
         self.lbl1.place(x=100, y=50)
         self.t1.place(x=1000, y=50)
         self.lbl11.place(x=130, y=80)
         self.lbl3.place(x=100, y=120)
         self.t3.place(x=1000, y=120)
+        self.t3_gen_frame.place(x=1130, y=120)
         self.lbl33.place(x=130, y=150)
-        self.lbl4.place(x=100, y=270)
-        self.t4.place(x=1000, y=270)
-        self.lbl44.place(x=130, y=300)
-        self.b1.place(x=570, y=330)
-        self.b2.place(x=640, y=330)
-        self.t5.place(x=200, y=370)
-        self.c1.place(x= 380, y=330)
-        self.lbt55.place(x=130, y=330)
-        self.et55.place(x=230, y=330)
 
 
 
+        self.lbl4.place(x=100, y=370)
+        self.t4.place(x=1000, y=370)
+        self.lbl44.place(x=130, y=400)
 
+        self.lbtdomain.place(x=130, y=435)
+        self.etdomain.place(x=230, y=435)
+
+        self.lbtfont_size.place(x=400, y=435)
+        self.etfont_size.place(x=500, y=435)
+        self.etfont_size.insert(END, str(self.text_font_size))
+
+        self.b1.place(x=570, y=470)
+        self.b2.place(x=640, y=470)
+
+        self.c1.place(x= 380, y=470)
+        self.lbt55.place(x=130, y=470)
+        self.et55.place(x=230, y=470)
+        self.t5.place(x=200, y=500)
+
+    def callback_gen_frame_file_mp4(self):
+        try:
+            folder_frame = self.file_mp4[:-4]+'_list_frames'
+            self.t5.configure(text=str("RUNNING " + str('GEN MP4 TO FRAME')), bg="gray", fg="blue")
+            if not os.path.exists(folder_frame):
+                os.makedirs(folder_frame)
+            mp4_origin = cv2.VideoCapture(self.file_mp4)
+            count_ret_frame = 1
+            while True:
+                ret, frame = mp4_origin.read()
+                if ret:
+                    cv2.imwrite(folder_frame+"/frame_"+str(count_ret_frame)+".png" ,frame)
+                    cv2.waitKey(1)
+                    count_ret_frame += 1
+                else:
+                    break
+            mp4_origin.release()
+
+            mp4_origin = cv2.VideoCapture(self.file_mp4)
+            width_origin = int(mp4_origin.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height_origin = int(mp4_origin.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = mp4_origin.get(cv2.CAP_PROP_FPS)
+            mp4_origin_with_frames = cv2.VideoWriter(self.file_mp4[:-4]+"_with_frame.mp4",
+                                                      cv2.VideoWriter_fourcc(*'MP4V'), fps,
+                                                      (width_origin, height_origin))
+            count_ret_frame = 1
+            text2 = "FRAME " + str(count_ret_frame)
+            font2 = ImageFont.truetype(cur_dir + "/dataset/font/" + "font_link.ttf", 100)
+            textsize2 = font2.getsize(text2)
+            # textX1 = (width_origin - textsize1[0]) / 2
+            # textY1 = (height_origin) / 2 - textsize1[1]
+            textX2 = (width_origin - textsize2[0]) / 2
+            textY2 = (height_origin) / 2 + 40
+            while True:
+                ret, frame = mp4_origin.read()
+                if ret:
+                    try:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                        pilim = Image.fromarray(frame)
+                        draw = ImageDraw.Draw(pilim)
+                        # draw.text((int(textX1), int(textY1 + opacity)), text1, font=font1)
+                        frame_count_text="FRAME " + str(count_ret_frame)
+                        draw.text((int(textX2), int(textY2 )), frame_count_text, font=font2)
+                        frame = np.array(pilim)
+                        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                        mp4_origin_with_frames.write(frame)
+                        cv2.waitKey(1)
+                        count_ret_frame=count_ret_frame+1
+                    except Exception as e:
+                        logging.debug(str(e))
+                else:
+                    break
+            mp4_origin.release()
+            mp4_origin_with_frames.release()
+
+            self.t5.configure(text=str("KETQUA:"))
+        except Exception as e:
+            logging.debug(str(e))
+            logging.debug("ERROR callback_gen_frame_file_mp4")
 
     def callback_folder_png(self):
         try:
@@ -266,6 +380,7 @@ class MyWindow:
             font2 = ImageFont.truetype(cur_dir + "/dataset/font/" + "font_link.ttf", 70)
             # text1 = "GET IT HERE"
             text2 = link_sp.replace('_', '/')
+            text2=self.domain+"/"+text2
             # textsize1 = font1.getsize(text1)
             textsize2 = font2.getsize(text2)
             # textX1 = (width_origin - textsize1[0]) / 2
@@ -331,8 +446,10 @@ class MyWindow:
             mp4_out.release()
             image.close()
             try:
-                os.rename(path_image, self.folder_output+"/DONE_IMAGE/"+link_sp+'.png')
-            except:
+                if os.path.exists(self.folder_output+"/DONE_IMAGE_" + str(self.run_count)+"/"+link_sp+'.png'):
+                    os.remove(self.folder_output+"/DONE_IMAGE_" + str(self.run_count)+"/"+link_sp+'.png')
+                os.rename(path_image, self.folder_output+"/DONE_IMAGE_" + str(self.run_count)+"/"+link_sp+'.png')
+            except Exception as e:
                 logging.debug(str(e))
             cv2.destroyAllWindows()
         except Exception as e:
@@ -361,14 +478,40 @@ class MyWindow:
 
     def start_run(self):
         try:
-            self.start_frame_image = int(self.et1.get())
-            self.end_frame_image = int(self.et2.get())
-            self.start_frame_text = int(self.et3.get())
-            self.end_frame_text = int(self.et4.get())
+            executor = ThreadPoolExecutor(1)
+            executor.submit(self.start_run_thread())
+        except Exception as e:
+            logging.debug(str(e))
+            logging.debug("SOME THING WRONG")
+        self.t5.configure(text=str("DONE ALL"), bg="gray", fg="white")
+    def start_run_thread(self):
+        try:
+            self.run_count = self.run_count+1
+            self.text_font_size = int(self.etfont_size.get())
+            # self.start_frame_image = int(self.et1.get())
+            # self.end_frame_image = int(self.et2.get())
+            self.start_open = int(self.et1open.get())
+            self.end_open = int(self.et2open.get())
+
+            self.start_model = int(self.et1model.get())
+            self.end_model = int(self.et2model.get())
+
+            self.start_close = int(self.et1close.get())
+            self.end_close = int(self.et2close.get())
+
+            self.start_frame_text = int(self.et1link.get())
+            self.end_frame_text = int(self.et2link.get())
             self.number_thread = int(self.et55.get())
 
-            if not os.path.exists(self.folder_output+'/DONE_IMAGE'):
-                os.makedirs(self.folder_output+'/DONE_IMAGE')
+            self.domain=str(self.etdomain.get())
+
+
+
+            # self.start_text = 241
+            # self.end_text = 473
+
+            if not os.path.exists(self.folder_output+'/DONE_IMAGE_'+str(self.run_count)):
+                os.makedirs(self.folder_output+'/DONE_IMAGE_'+str(self.run_count))
             # print(self.is_new_background.get())
 
 
@@ -436,5 +579,5 @@ except Exception as e:
 mywin = MyWindow(window)
 
 window.title('CONVERT IMAGE TO VIDEO')
-window.geometry("1300x450+10+10")
+window.geometry("1300x600+10+10")
 window.mainloop()
